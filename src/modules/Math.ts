a48da57cd5abfb7eebb0e6afeb7940c9d0e11a72
Cassio Moraes <taykonbrkz@gmail.com>
2023-11-23 20:36:36 -0300

    Revert "switched to typescript"
    This reverts commit 23907f458c9d93caffbb36a9f76ffc27735454d4.

--- src/modules/Math.ts
+++
@@ -1,8 +0,0 @@
-
-export const soma = (item1: string, item2: string) => {
-  let num1 = Number(item1);
-  let num2 = Number(item2);
-  if (Number.isNaN(Number(num1))) num1 = 0;
-  if (Number.isNaN(Number(num2))) num2 = 0;
-  return (num1 + num2).toFixed(1);
-};
